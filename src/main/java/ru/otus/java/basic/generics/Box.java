3c9fe938eb157b44bf981e6cb203609cf53ac1c8
Alexey Sidorov <alexey.sidorov.dev@mail.ru>
2025-02-17 10:46:49 +0300

    fix after code review

--- src/main/java/ru/otus/java/basic/generics/Box.java
+++ src/main/java/ru/otus/java/basic/generics/Box.java
@@ -1,41 +1,44 @@
 package ru.otus.java.basic.generics;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Box<T extends Fruit> {
 
-  private final List<Fruit> fruits = new ArrayList<>();
+  private final List<T> fruits;
 
-  public void add(Fruit fruit) {
+  public Box() {
+    fruits = new ArrayList<>();
+  }
+
+  public void add(T fruit) {
     fruits.add(fruit);
   }
 
-  public void add(T fruit, int count) {
-    for (int i = 0; i < count; i++) {
-      fruits.add(fruit);
-    }
+  public void add(T[] fruits) {
+    Arrays.stream(fruits).forEach(this::add);
   }
 
   public int calculateWeight() {
     int weight = 0;
-    for (Fruit item : fruits
+    for (Fruit fruit : fruits
     ) {
-      weight += item.getWeight();
+      weight += fruit.getWeight();
     }
     return weight;
   }
 
-  public boolean compare(Box<? extends Fruit> box) {
-    return this.calculateWeight() == box.calculateWeight();
+  public boolean compare(Box<?> box) {
+    return calculateWeight() == box.calculateWeight();
   }
 
-  public void refill(Box<? extends Fruit> box) {
-    for (Fruit fruit : this.fruits
-    ) {
-      box.add(fruit);
+  public void refill(Box<? super T> box) {
+    if (this != box) {
+      for (T fruit : fruits) {
+        box.add(fruit);
+      }
+      fruits.clear();
     }
-
-    this.fruits.clear();
   }
 }
--- src/test/java/ru/otus/java/basic/generics/BoxTest.java
+++ src/test/java/ru/otus/java/basic/generics/BoxTest.java
@@ -7,91 +7,123 @@ class BoxTest {
 
   int appleWeight = 2;
   int orangeWeight = 3;
-  Apple apple = new Apple(appleWeight);
-  Orange orange = new Orange(orangeWeight);
+
 
   @Test
   void testAdd() {
+    Apple apple1 = new Apple(appleWeight);
+    Orange orange1 = new Orange(orangeWeight);
+    Apple apple2 = new Apple(appleWeight);
+    Orange orange2 = new Orange(orangeWeight);
 
-    Assertions.assertEquals(appleWeight, apple.getWeight());
-    Assertions.assertEquals(orangeWeight, orange.getWeight());
+    Assertions.assertEquals(appleWeight, apple1.getWeight());
+    Assertions.assertEquals(orangeWeight, orange1.getWeight());
 
     Box<Fruit> fruitBox = new Box<>();
-    Assertions.assertDoesNotThrow(() -> fruitBox.add(apple));
-    Assertions.assertDoesNotThrow(() -> fruitBox.add(orange));
+    Assertions.assertDoesNotThrow(() -> fruitBox.add(apple1));
+    Assertions.assertDoesNotThrow(() -> fruitBox.add(orange1));
+    Assertions.assertDoesNotThrow(() -> fruitBox.add(new Apple[]{apple2}));
+    Assertions.assertDoesNotThrow(() -> fruitBox.add(new Orange[]{orange2}));
 
     Box<Apple> appleBox = new Box<>();
-    Assertions.assertDoesNotThrow(() -> appleBox.add(apple));
+    Assertions.assertDoesNotThrow(() -> appleBox.add(apple1));
+    Assertions.assertDoesNotThrow(() -> appleBox.add(new Apple[]{apple2}));
 
     Box<Orange> orangeBox = new Box<>();
-    Assertions.assertDoesNotThrow(() -> orangeBox.add(orange));
+    Assertions.assertDoesNotThrow(() -> orangeBox.add(orange1));
+    Assertions.assertDoesNotThrow(() -> orangeBox.add(new Orange[]{orange2}));
   }
 
   @Test
   public void testCalculateWeight() {
+    Apple apple1 = new Apple(appleWeight);
+    Orange orange1 = new Orange(orangeWeight);
+    Apple apple2 = new Apple(appleWeight);
+    Orange orange2 = new Orange(orangeWeight);
+
     Box<Fruit> fruitBox = new Box<>();
-    fruitBox.add(apple);
-    fruitBox.add(orange);
+    fruitBox.add(apple1);
+    fruitBox.add(orange1);
     Assertions.assertEquals(appleWeight + orangeWeight, fruitBox.calculateWeight());
-    fruitBox.add(apple, 2);
-    fruitBox.add(orange, 2);
-    Assertions.assertEquals(3 * appleWeight + 3 * orangeWeight, fruitBox.calculateWeight());
+    fruitBox.add(new Apple[]{apple2});
+    fruitBox.add(new Orange[]{orange2});
+    Assertions.assertEquals(2 * appleWeight + 2 * orangeWeight, fruitBox.calculateWeight());
 
     Box<Apple> appleBox = new Box<>();
-    appleBox.add(apple);
-    appleBox.add(apple);
+    appleBox.add(apple1);
+    Assertions.assertEquals(appleWeight, appleBox.calculateWeight());
+    appleBox.add(new Apple[]{apple2});
     Assertions.assertEquals(2 * appleWeight, appleBox.calculateWeight());
-    appleBox.add(apple, 2);
-    Assertions.assertEquals(4 * appleWeight, appleBox.calculateWeight());
 
     Box<Orange> orangeBox = new Box<>();
-    orangeBox.add(orange);
-    orangeBox.add(orange);
+    orangeBox.add(orange1);
+    Assertions.assertEquals(orangeWeight, orangeBox.calculateWeight());
+    orangeBox.add(new Orange[]{orange2});
     Assertions.assertEquals(2 * orangeWeight, orangeBox.calculateWeight());
-    orangeBox.add(orange, 2);
-    Assertions.assertEquals(4 * orangeWeight, orangeBox.calculateWeight());
-
   }
 
   @Test
   public void testCompare() {
-    Box<Fruit> fruitBox = new Box<>();
-    fruitBox.add(apple);
-    fruitBox.add(orange, 1);
+    Apple apple1 = new Apple(appleWeight);
+    Apple apple2 = new Apple(appleWeight);
+    Apple apple3 = new Apple(appleWeight);
+    Orange orange1 = new Orange(orangeWeight);
+    Orange orange2 = new Orange(orangeWeight);
 
+    Box<Fruit> fruitBox1 = new Box<>();
+    fruitBox1.add(apple1);
+    fruitBox1.add(orange1);
     Box<Apple> appleBox = new Box<>();
-    appleBox.add(apple, 3);
-
+    appleBox.add(apple1);
     Box<Orange> orangeBox = new Box<>();
-    orangeBox.add(orange, 2);
-
-    Assertions.assertTrue(appleBox.compare(orangeBox));
-    Assertions.assertTrue(orangeBox.compare(appleBox));
+    orangeBox.add(orange1);
+    Assertions.assertFalse(appleBox.compare(orangeBox));
+    Assertions.assertFalse(orangeBox.compare(appleBox));
+    Assertions.assertFalse(fruitBox1.compare(orangeBox));
+    Assertions.assertFalse(fruitBox1.compare(appleBox));
+    Assertions.assertFalse(orangeBox.compare(fruitBox1));
+    Assertions.assertFalse(appleBox.compare(fruitBox1));
     Assertions.assertTrue(orangeBox.compare(orangeBox));
     Assertions.assertTrue(appleBox.compare(appleBox));
-    Assertions.assertTrue(fruitBox.compare(fruitBox));
+    Assertions.assertTrue(fruitBox1.compare(fruitBox1));
 
-    Assertions.assertFalse(fruitBox.compare(appleBox));
-    Assertions.assertFalse(fruitBox.compare(orangeBox));
-    Assertions.assertFalse(appleBox.compare(fruitBox));
-    Assertions.assertFalse(orangeBox.compare(fruitBox));
+    Box<Fruit> fruitBox2 = new Box<>();
+    fruitBox2.add(apple1);
+    Assertions.assertTrue(fruitBox2.compare(appleBox));
+    Assertions.assertTrue(appleBox.compare(fruitBox2));
+
+    appleBox.add(apple2);
+    appleBox.add(apple3);
+    orangeBox.add(orange2);
+    Assertions.assertTrue(appleBox.compare(orangeBox));
   }
 
   @Test
   public void testRefill() {
-    Box<Fruit> fruitBox1 = new Box<>();
-    fruitBox1.add(apple);
-    fruitBox1.add(orange);
-
+    Apple apple1 = new Apple(appleWeight);
+    Apple apple2 = new Apple(appleWeight);
+    Orange orange1 = new Orange(orangeWeight);
+    Orange orange2 = new Orange(orangeWeight);
     Box<Apple> appleBox1 = new Box<>();
-    appleBox1.add(apple, 2);
-
+    Box<Fruit> fruitBox1 = new Box<>();
     Box<Orange> orangeBox1 = new Box<>();
-    orangeBox1.add(orange, 2);
-
     Box<Apple> appleBox2 = new Box<>();
     Box<Fruit> fruitBox2 = new Box<>();
     Box<Orange> orangeBox2 = new Box<>();
+
+    fruitBox1.add(apple1);
+    fruitBox1.add(orange1);
+    appleBox1.add(apple1);
+    appleBox1.add(apple2);
+    orangeBox1.add(orange1);
+    orangeBox1.add(orange2);
+
+    fruitBox1.refill(fruitBox1);
+    Assertions.assertEquals(appleWeight + orangeWeight, fruitBox1.calculateWeight());
+    appleBox1.refill(appleBox1);
+    Assertions.assertEquals(2 * appleWeight, appleBox1.calculateWeight());
+    orangeBox1.refill(orangeBox1);
+    Assertions.assertEquals(2 * orangeWeight, orangeBox1.calculateWeight());
 
     appleBox1.refill(appleBox2);
     Assertions.assertEquals(2 * appleWeight, appleBox2.calculateWeight());
